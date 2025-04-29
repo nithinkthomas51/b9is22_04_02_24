ae56a5a0f0320719ab19a4717f465b11648d87cd
Nithin K Thomas <64864340+nithinkthomas51@users.noreply.github.com>
2025-04-29 19:38:36 +0100

    Refactored previous code and completed exercise 3

--- index.js
+++ index.js
@@ -5,12 +5,26 @@ let c=() => {
     }
 }
 
-let findSum = () => {
+let sumOfMultiplesof3or5Below1000 = () => {
+    let sum = sumOfMultiplesOfAorBBelowN(3, 5, 1000);
+    alert(sum);
+}
+
+let sumOfMultiplesOfAorBBelowN = (a, b, n) => {
     let sum = 0;
-    for (let i = 1; i < 1000; i++) {
-        if (i % 3 === 0 || i % 5 === 0) {
+    for (let i = 1; i < n; i++) {
+        if (i % a === 0 || i % b === 0) {
             sum += i;
         }
     }
+    return sum;
+}
+
+let findSum = () => {
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
+    let n = document.getElementById('n').value;
+
+    let sum = sumOfMultiplesOfAorBBelowN(a, b, n);
     alert(sum);
 }
