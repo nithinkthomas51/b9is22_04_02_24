8045ae5f675ea061e540812345407778ef930ff9
Nithin K Thomas <64864340+nithinkthomas51@users.noreply.github.com>
2025-04-29 19:16:56 +0100

    Completed exercise 2

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ let c=() => {
         alert("Baggage is overweight")
     }
 }
+
+let findSum = () => {
+    let sum = 0;
+    for (let i = 1; i < 1000; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+    alert(sum);
+}
